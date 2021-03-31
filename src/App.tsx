aa9846e417fadd96b82d8ae4d0a390af3cd816ef
TinaGorna <kristina.gorbunova1997@gmail.com>
2021-03-31 13:30:58 +0300

    Rating/Accordion files added

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,15 @@
 import React from "react";
 import "./App.module.css";
+import Accordion from "./components/Accordion/Accordion"
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
   return (
       <div className="App">
-        <Rating/>
+        <Rating value = {3}/>
         <Accordion/>
       </div>
   );
-}
-
-function Accordion() {
-  return (
-      <div>
-        <AccordionTitle/>
-        <AccordionBody/>
-      </div>
-  )
-}
-
-function AccordionTitle() {
-  return (
-      <div>
-        <h3>Menu</h3>
-      </div>
-  )
-}
-function AccordionBody () {
-  return (
-      <div>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-        </ul>
-      </div>
-  )
-}
-
-function Rating() {
-  return (
-      <div>
-        <Star/>
-        <Star/>
-        <Star/>
-        <Star/>
-        <Star/>
-      </div>
-  )
-}
-function Star() {
-  return <div>Star</div>
 }
 
 
